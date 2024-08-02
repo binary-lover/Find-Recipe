cdd887c6e8cbe7e551ac392050de6d6944d6d1f8
binary-lover <whitedevil.hamd@gmail.com>
2024-08-02 10:44:10 +0530

    ui design completed

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,26 +1,44 @@
 import { Search } from "lucide-react";
 import React from "react";
+import Recipiecard from "../Recipiecard";
 
 const HomePage = () => {
   return (
     <div className="bg-[#faf9fb] p-10 flex-1">
       <div className="max-w-screen-lg mx-auto">
-        <form >
-          <label className="input shadow-md flex items-center gap-2" style={{outline: "none"}}>
+        <form>
+          <label
+            className="input shadow-md flex items-center gap-2"
+            style={{ outline: "none" }}
+          >
             <Search size={"24"} />
             <input
               type="text"
               className="text-sm md:text-md grow border-red-300"
-              placeholder="what do you want to cook today?" 
+              placeholder="what do you want to cook today?"
             />
           </label>
         </form>
         <h1 className="font-bold text-3xl md:text-5xl mt-4">
-          Reccomended recepie 
+          Reccomended recepie
         </h1>
         <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tighter">
           Popular recepies
         </p>
+
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {/* Recipie card */}
+          <Recipiecard />
+          <Recipiecard />
+          <Recipiecard />
+          <Recipiecard />
+          <Recipiecard />
+          <Recipiecard />
+          <Recipiecard />
+          <Recipiecard />
+          <Recipiecard />
+
+        </div>
       </div>
     </div>
   );
